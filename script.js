a5665d83554fcf19c1bc515fb63c2c62321efe21
Anastasia <tgmonresu@gmail.com>
2023-02-24 01:06:05 +0300

    semi

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const RED = "#f00";
 const GREEN = "#0f0";
 const bluE = "#00000ff";
 
-console.log(RED)
-console.log(GREEN)
-console.log(bluE)
+console.log(RED);
+console.log(GREEN);
+console.log(bluE);
 
 isColor("color");
