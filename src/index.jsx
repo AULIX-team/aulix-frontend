ee6213ddbe94dfd7138311263f8499c53c463d7b
Никита Шипилов <116575516+LAYT73@users.noreply.github.com>
2023-02-25 00:23:22 +0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+ <React.StrictMode>
     
   </React.StrictMode>
 );
