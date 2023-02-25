0793f7982a461e2d2474baef4393a19bfb1921bf
nimainikita <nimainikita@gmail.com>
2023-02-25 16:11:54 +0300

    Начало работы

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>Begining...</div>
+  );
+};
 
-export default App
+export default App;
