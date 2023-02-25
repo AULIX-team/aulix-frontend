2c8e78583d5faab64220576f34110ffeeffafb7b
LAYT73 <nsshipilov@gmail.com>
2023-02-26 02:03:10 +0400

    финал

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,18 +1,9 @@
 import React from "react";
-import Container from "../shared/ui/Layout/Container";
-import RelatedProducts from "../pages/DetailPage/RelatedProducts/RelatedProducts";
-import CardOfProduct from "../pages/DetailPage/CardOfProduct/CardOfProduct";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <Container>
-        <CardOfProduct />
-      </Container>
-      <Container>
-        <RelatedProducts />
-      </Container>
     </div>
   );
 };
